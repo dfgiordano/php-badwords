727a312b4f092700edb6313b95f9f9277f3405be
dfgiordano <federico.giordano90@gmail.com>
2021-06-28 10:19:09 +0200

    ok

--- index.php
+++ index.php
@@ -13,10 +13,21 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
 
     $newParagraph = str_replace($_GET["string"],"***",$paragraph);
     $newParagraphLength = strlen($newParagraph);
-    
-
 ?>
 
-<p style= "margin: 50px;"> <?php echo "Paragrafo originale : " . $paragraph . " " . $paragraphLength; ?> </p>
 
-<p style= "margin: 50px;"> <?php echo "Paragrafo modificato : " . $newParagraph . " " . "Lunghezza : " . $newParagraphLength ?> </p>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    <p style= "margin: 50px;"> <?php echo "Paragrafo originale : " . $paragraph . " " . "$paragraphLength" ; ?> </p>
+
+    <p style= "margin: 50px;"> <?php echo "Paragrafo modificato : " . $newParagraph . " " . "Lunghezza : " . "$newParagraphLength" ; ?> </p> 
+    </body>
+</html>
